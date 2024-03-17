d04b50e1d0c84abb06dbccf1fbaf26497af8ed4c
sodtiruk <sodtiruk22@gmail.com>
2024-03-18 05:56:22 +0700

    add sfx and script scale up player

--- Assets/Mygame/Scripts/Correctable.cs
+++ Assets/Mygame/Scripts/Correctable.cs
@@ -1,13 +1,22 @@
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Correctable : MonoBehaviour
+
 {
     // Start is called before the first frame update
+    public GameObject player;
+    public AudioSource sfx;
     void Start()
-    {
+    { 
+        player = GameObject.FindWithTag("Player");
         
+        // find gameObject sound
+        // and then keep Gameobject into sfx
+        GameObject soundObject = GameObject.Find("BurgerSfx");
+        sfx = soundObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,6 +29,8 @@ public class Correctable : MonoBehaviour
     {
        if (other.gameObject.tag == "Player") // check tag
         {
+            sfx.Play();
+            player.transform.localScale = new Vector3(player.transform.localScale.x + 1f, player.transform.localScale.y + 1f, player.transform.localScale.z + 1f);
             Destroy(gameObject);
         } 
     }
